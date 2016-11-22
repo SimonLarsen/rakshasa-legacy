260c69078537b35c8464b99e6597a0a403b07ba6
Simon Larsen <simonhffh@gmail.com>
2016-11-22 20:13:30 +0100

    Added test level using new format

--- data/levels/1.lua
+++ data/levels/1.lua
@@ -8,13 +8,13 @@ return {
   height = 30,
   tilewidth = 16,
   tileheight = 16,
-  nextobjectid = 49,
+  nextobjectid = 83,
   properties = {},
   tilesets = {},
   layers = {
     {
       type = "objectgroup",
-      name = "Object Layer 1",
+      name = "wave1",
       visible = true,
       opacity = 1,
       offsetx = 0,
@@ -23,53 +23,295 @@ return {
       properties = {},
       objects = {
         {
-          id = 45,
+          id = 60,
+          name = "0",
+          type = "ship",
+          shape = "polyline",
+          x = 159.333,
+          y = -48,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 1.33333, y = 207.334 },
+            { x = 1.33333, y = 303.334 },
+            { x = 1.33333, y = 562 }
+          },
+          properties = {}
+        },
+        {
+          id = 61,
+          name = "0.3",
+          type = "ship",
+          shape = "polyline",
+          x = 210,
+          y = -46.6667,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -0.666667, y = 228.667 },
+            { x = -0.666667, y = 324.667 },
+            { x = 1.33333, y = 562 }
+          },
+          properties = {}
+        },
+        {
+          id = 59,
+          name = "0",
+          type = "ship",
+          shape = "polyline",
+          x = 108.667,
+          y = -45.3333,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 2.84217e-14, y = 182.667 },
+            { x = 2.84217e-14, y = 278.667 },
+            { x = 1.33333, y = 562 }
+          },
+          properties = {}
+        },
+        {
+          id = 62,
+          name = "0.3",
+          type = "ship",
+          shape = "polyline",
+          x = 261.333,
+          y = -47.3333,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -0.666667, y = 254.667 },
+            { x = -0.666667, y = 350.667 },
+            { x = 1.33333, y = 562 }
+          },
+          properties = {}
+        },
+        {
+          id = 57,
+          name = "0",
+          type = "ship",
+          shape = "polyline",
+          x = 52,
+          y = -46.6667,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -1.33333, y = 153.333 },
+            { x = -1.33333, y = 249.333 },
+            { x = 1.33333, y = 562 }
+          },
+          properties = {}
+        },
+        {
+          id = 63,
+          name = "1",
+          type = "drone_spawner",
+          shape = "polyline",
+          x = -24,
+          y = 159.333,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 372, y = 0 }
+          },
+          properties = {
+            ["count"] = 15,
+            ["delay"] = 0.2
+          }
+        },
+        {
+          id = 65,
+          name = "2",
+          type = "temple",
+          shape = "polyline",
+          x = 83.3333,
+          y = -32.6667,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 0, y = 544 }
+          },
+          properties = {}
+        },
+        {
+          id = 67,
+          name = "0.5",
+          type = "ship",
+          shape = "polyline",
+          x = 160.667,
+          y = -36,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 0, y = 259.333 },
+            { x = -0.666667, y = 356 },
+            { x = 0, y = 547.333 }
+          },
+          properties = {}
+        },
+        {
+          id = 68,
+          name = "0.5",
+          type = "ship",
+          shape = "polyline",
+          x = 242.667,
+          y = -37.3333,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -1.33333, y = 222 },
+            { x = -2.66667, y = 357.333 },
+            { x = -4.66667, y = 547.333 }
+          },
+          properties = {}
+        },
+        {
+          id = 73,
+          name = "1",
+          type = "drone_spawner",
+          shape = "polyline",
+          x = 352,
+          y = 112,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = false,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -128, y = 0 },
+            { x = -208, y = 80 },
+            { x = -384, y = 80 }
+          },
+          properties = {
+            ["count"] = 10,
+            ["delay"] = 0.2
+          }
+        }
+      }
+    },
+    {
+      type = "objectgroup",
+      name = "wave2",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "index",
+      properties = {},
+      objects = {
+        {
+          id = 75,
           name = "0",
           type = "laser",
           shape = "polyline",
-          x = 70.5,
-          y = 45.5,
+          x = 50,
+          y = 43.3333,
           width = 0,
           height = 0,
           rotation = 0,
           visible = true,
           polyline = {
             { x = 0, y = 0 },
-            { x = 0, y = 80 }
+            { x = 80, y = 80 }
           },
           properties = {}
         },
         {
-          id = 46,
-          name = "4",
+          id = 76,
+          name = "2",
           type = "laser",
           shape = "polyline",
-          x = 174.5,
-          y = 82,
+          x = 190,
+          y = 185.333,
           width = 0,
           height = 0,
           rotation = 0,
           visible = true,
           polyline = {
             { x = 0, y = 0 },
-            { x = 96, y = 0 }
+            { x = 80, y = -80 }
           },
           properties = {}
         },
         {
-          id = 48,
-          name = "4",
-          type = "laser",
+          id = 77,
+          name = "2",
+          type = "ship",
           shape = "polyline",
-          x = 127,
-          y = 220.5,
+          x = 218,
+          y = -26.6667,
           width = 0,
           height = 0,
           rotation = 0,
           visible = true,
           polyline = {
             { x = 0, y = 0 },
-            { x = 64, y = -64 }
+            { x = -0.666667, y = 204.667 },
+            { x = -0.666667, y = 318.667 },
+            { x = -1.33333, y = 530.667 }
+          },
+          properties = {}
+        },
+        {
+          id = 79,
+          name = "1",
+          type = "silo",
+          shape = "polyline",
+          x = 76.6667,
+          y = -33.3333,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 0.666667, y = 531.333 }
+          },
+          properties = {}
+        },
+        {
+          id = 82,
+          name = "0.6",
+          type = "ship",
+          shape = "polyline",
+          x = 147.333,
+          y = -32.6667,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -0.666667, y = 204.667 },
+            { x = -0.666667, y = 318.667 },
+            { x = -1.33333, y = 530.667 }
           },
           properties = {}
         }
@@ -77,7 +319,7 @@ return {
     },
     {
       type = "objectgroup",
-      name = "Object Layer 2",
+      name = "boss",
       visible = false,
       opacity = 1,
       offsetx = 0,
@@ -86,14 +328,14 @@ return {
       properties = {},
       objects = {
         {
-          id = 15,
-          name = "2",
-          type = "durga",
+          id = 70,
+          name = "0",
+          type = "agni",
           shape = "rectangle",
-          x = 69.3333,
-          y = 36.6667,
-          width = 185.333,
-          height = 106.667,
+          x = 162.667,
+          y = 119.333,
+          width = 0,
+          height = 0,
           rotation = 0,
           visible = true,
           properties = {}
