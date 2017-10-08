851d4a542fa6454cb2aa897c05a2bf29097aee05
Simon Larsen <simonhffh@gmail.com>
2017-10-08 21:45:49 +0200

    Level work

--- data/levels/2-1.lua
+++ data/levels/2-1.lua
@@ -8,7 +8,7 @@ return {
   height = 30,
   tilewidth = 16,
   tileheight = 16,
-  nextobjectid = 7,
+  nextobjectid = 57,
   properties = {},
   tilesets = {},
   layers = {
@@ -23,55 +23,57 @@ return {
       properties = {},
       objects = {
         {
-          id = 1,
+          id = 35,
           name = "0",
           type = "Laser",
           shape = "polyline",
           x = 16,
-          y = 256,
+          y = 240,
           width = 0,
           height = 0,
           rotation = 0,
           visible = true,
           polyline = {
             { x = 0, y = 0 },
-            { x = 144, y = 0 }
+            { x = 80, y = 0 }
           },
           properties = {}
         },
         {
-          id = 2,
-          name = "3",
+          id = 36,
+          name = "0",
           type = "Laser",
           shape = "polyline",
           x = 304,
-          y = 176,
+          y = 240,
           width = 0,
           height = 0,
           rotation = 0,
           visible = true,
           polyline = {
             { x = 0, y = 0 },
-            { x = -128, y = 0 }
+            { x = -80, y = 0 }
           },
           properties = {}
         },
         {
-          id = 3,
-          name = "3",
-          type = "Laser",
+          id = 31,
+          name = "2",
+          type = "Silo",
           shape = "polyline",
-          x = 16,
-          y = 96,
+          x = 352,
+          y = 80,
           width = 0,
           height = 0,
           rotation = 0,
           visible = true,
           polyline = {
             { x = 0, y = 0 },
-            { x = 144, y = 0 }
+            { x = -384, y = 0 }
           },
-          properties = {}
+          properties = {
+            ["speed"] = 140
+          }
         }
       }
     },
@@ -86,11 +88,164 @@ return {
       properties = {},
       objects = {
         {
-          id = 5,
+          id = 14,
           name = "0",
-          type = "Temple",
+          type = "Laser",
+          shape = "polyline",
+          x = 128,
+          y = 240,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = -16, y = 0 },
+            { x = 80, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          id = 16,
+          name = "0.2",
+          type = "Fighter",
+          shape = "polyline",
+          x = 272,
+          y = -32,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 0, y = 128 },
+            { x = 0, y = 240 },
+            { x = 0, y = 544 }
+          },
+          properties = {}
+        },
+        {
+          id = 15,
+          name = "0.1",
+          type = "Fighter",
+          shape = "polyline",
+          x = 48,
+          y = -32,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 0, y = 128 },
+            { x = 0, y = 240 },
+            { x = 0, y = 544 }
+          },
+          properties = {}
+        },
+        {
+          id = 17,
+          name = "2.5",
+          type = "Laser",
+          shape = "polyline",
+          x = 16,
+          y = 176,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 80, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          id = 18,
+          name = "0",
+          type = "Laser",
+          shape = "polyline",
+          x = 304,
+          y = 176,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = -80, y = 0 }
+          },
+          properties = {}
+        },
+        {
+          id = 25,
+          name = "1",
+          type = "Bunker",
           shape = "rectangle",
-          x = 176,
+          x = 160,
+          y = 144,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 26,
+          name = "0.32",
+          type = "Bunker",
+          shape = "rectangle",
+          x = 128,
+          y = 128,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 27,
+          name = "0",
+          type = "Bunker",
+          shape = "rectangle",
+          x = 192,
+          y = 128,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 30,
+          name = "1",
+          type = "Turret",
+          shape = "rectangle",
+          x = 160,
+          y = 64,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    },
+    {
+      type = "objectgroup",
+      name = "3",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 56,
+          name = "0",
+          type = "LaserTurret",
+          shape = "rectangle",
+          x = 160,
           y = 96,
           width = 0,
           height = 0,
